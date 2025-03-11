6a5d6ecd357fde6cdd37f7f2d54bafe880fd65b6
George20030412 <97119283@st.deltion.nl>
2025-03-11 13:36:54 +0100

    Load menu

---
+++ Frozen in Fear/Assets/Scripts/ScenLoader.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("Scene 0");
+        // Replace with your scene
+    }
+}
