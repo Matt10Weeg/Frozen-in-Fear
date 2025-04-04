c6182e2c693f72f57bf8d27f96bd6b9f7166e87b
Matthew <matthewweegman@gmail.com>
2025-04-04 17:14:25 +0200

    scripts in

---
+++ Frozen in Fear/Assets/Goeie inv/Item.cs
@@ -0,0 +1,48 @@
+
+Item
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string ItemName = "Zwaard";
+    public Sprite ItemIcon;
+    private bool isPickedUp = false;
+
+    public bool IsPickedUp
+    {
+        get { return isPickedUp; }
+    }
+
+    public string ItemNameProperty
+    {
+        get { return ItemName; }
+    }
+
+    public void PickUp(Transform hand)
+    {
+        isPickedUp = true;
+        EquipToHand(hand);
+    }
+
+    private void EquipToHand(Transform hand)
+    {
+        transform.SetParent(hand);
+        transform.localPosition = new Vector3(0, 0, 0.2f);
+        transform.localRotation = Quaternion.Euler(180, 270, 50);
+        transform.localScale = Vector3.one * 0.5f;
+        gameObject.SetActive(true);
+        Debug.Log("Zwaard in hand gezet! Ouder: " + transform.parent.name +
+                  ", Local Pos: " + transform.localPosition +
+                  ", World Pos: " + transform.position +
+                  ", Scale: " + transform.localScale);
+    }
+
+    public void Drop(Vector3 position)
+    {
+        isPickedUp = false;
+        transform.SetParent(null);
+        transform.SetPositionAndRotation(position, Quaternion.identity);
+        gameObject.SetActive(true);
+        Debug.Log("Zwaard laten vallen!");
+    }
+}
